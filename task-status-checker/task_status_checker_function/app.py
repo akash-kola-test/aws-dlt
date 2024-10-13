f425c48401639af714fe1e1dc8d916dbb0b53514
Akash Kola <akash.nani@datagrokr.com>
2024-10-14 01:59:54 +0530

    Fixed issues in test status checker

--- task-status-checker/task_status_checker_function/app.py
+++ task-status-checker/task_status_checker_function/app.py
@@ -1,10 +1,11 @@
 import boto3
 import logging
-from typing import List
+from typing import Any, Dict, Optional
 
 # Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, _):
     logger.info("Received event: %s", event)
@@ -16,35 +17,48 @@ def lambda_handler(event, _):
 
     ecs = boto3.client("ecs", region_name=region)
 
-    logger.info("Retrieving task ARNs for cluster: %s", cluster)
-    tasksArns = get_tasks_arns(ecs, cluster)
-    
-    if not tasksArns:
-        logger.info("No tasks found for cluster: %s", cluster)
-        event["isRunning"] = is_running
-        return event
+    nextToken = None
 
-    logger.info("Found task ARNs: %s", tasksArns)
-    tasks_result = ecs.describe_tasks(cluster=cluster, tasks=tasksArns)
-    tasks = tasks_result.get("tasks", []) or []
-    
-    logger.info("Described tasks: %s", tasks)
-    test_tasks = filter(lambda task: task["group"] == test_id, tasks)
+    while True:
+        logger.info("Retrieving task ARNs for cluster: %s", cluster)
+        tasks = list_tasks(ecs, cluster, nextToken)
+        tasks_arns = tasks.get("taskArns", [])
+        logger.info("Found task ARNs: %s", tasks_arns)
 
-    if any(test_tasks):
-        is_running = True
-        logger.info("Test tasks are running for test_id: %s", test_id)
-    else:
-        logger.info("No running tasks found for test_id: %s", test_id)
+        if len(tasks_arns) != 0:
+            tasks_result = ecs.describe_tasks(cluster=cluster, tasks=tasks_arns)
+            described_tasks = tasks_result.get("tasks", []) or []
+            logger.info("Described tasks: %s", tasks)
+
+            test_tasks = filter(lambda task: task["group"] == test_id, described_tasks)
+
+            if any(test_tasks):
+                is_running = True
+                logger.info("Test tasks are running for test_id: %s", test_id)
+
+        nextToken = tasks["nextToken"]
+        logger.info("Next token for listing tasks: %s", nextToken)
+
+        if not nextToken:
+            break
 
     event["isRunning"] = is_running
     logger.info("Returning event: %s", event)
     return event
 
 
-def get_tasks_arns(ecs, cluster_name: str) -> List[str]:
+def list_tasks(
+    ecs, cluster_name: str, next_token: Optional[str] = None
+) -> Dict[str, Any]:
     logger.info("Listing tasks for cluster: %s", cluster_name)
-    tasks_result = ecs.list_tasks(cluster=cluster_name)
-    tasksArns = tasks_result.get("taskArns", [])
-    logger.info("Task ARNs retrieved: %s", tasksArns)
-    return tasksArns
+    params = {"cluster": cluster_name}
+
+    if next_token:
+        params["nextToken"] = next_token
+
+    tasks_result = ecs.list_tasks(**params)
+    tasks_arns = tasks_result.get("taskArns", [])
+    next_token = tasks_result.get("nextToken")
+    logger.info("Task ARNs retrieved: %s", tasks_arns)
+
+    return {"taskArns": tasks_arns, "nextToken": next_token}
--- task-status-checker/tests/test_app.py
+++ task-status-checker/tests/test_app.py
@@ -1,58 +1,146 @@
-from task_status_checker_function.app import lambda_handler, get_tasks_arns
-from unittest.mock import patch
+from task_status_checker_function.app import lambda_handler, list_tasks
+from unittest.mock import Mock, patch, call
 
 
 @patch("boto3.client")
-def test_get_tasks_arns(mock_boto_client):
+def test_list_tasks(mock_boto_client):
     mock_ecs = mock_boto_client.return_value
-    mock_ecs.list_tasks.return_value = {"taskArns": ["1", "2"]}
+    expected_task_arns = ["1", "2"]
+    next_token = None
+    mock_ecs.list_tasks.return_value = {
+        "taskArns": expected_task_arns,
+        "nextToken": next_token,
+    }
 
     cluster = "cluster"
-    taskArns = get_tasks_arns(mock_ecs, cluster)
+    tasks = list_tasks(mock_ecs, cluster)
 
-    assert taskArns == ["1", "2"]
+    assert tasks["taskArns"] == expected_task_arns
+
+
+@patch("boto3.client")
+def test_list_tasks_with_next_token(mock_boto_client):
+    mock_ecs = mock_boto_client.return_value
+    list_tasks_mock: Mock = Mock()
+    mock_ecs.list_tasks = list_tasks_mock
+
+    expected_first_list_task_arns = ["1", "2"]
+    expected_first_next_token = "some token"
+
+    expected_second_list_task_arns = ["3", "4"]
+    expected_second_next_token = None
+
+    mock_ecs.list_tasks.side_effect = [
+        {
+            "taskArns": expected_first_list_task_arns,
+            "nextToken": expected_first_next_token,
+        },
+        {
+            "taskArns": expected_second_list_task_arns,
+            "nextToken": expected_second_next_token,
+        },
+    ]
+
+    cluster = "cluster"
+    tasks = list_tasks(mock_ecs, cluster)
+    next_tasks = list_tasks(mock_ecs, cluster, expected_first_next_token)
+
+    assert tasks["taskArns"] == expected_first_list_task_arns
+    assert tasks["nextToken"] == expected_first_next_token
+    assert next_tasks["taskArns"] == expected_second_list_task_arns
+    assert next_tasks["nextToken"] is expected_second_next_token
+
+    list_tasks_mock.assert_has_calls(
+        [
+            call(cluster=cluster),
+            call(cluster=cluster, nextToken=expected_first_next_token),
+        ]
+    )
 
 
 @patch("boto3.client")
 def test_lambda_handler(mock_boto_client):
     mock_ecs = mock_boto_client.return_value
-    mock_ecs.list_tasks.return_value = {"taskArns": ["1", "2"]}
+
+    expected_task_arns = ["1", "2"]
+    next_token = None
+    expected_is_running = False
+
+    mock_ecs.list_tasks.return_value = {
+        "taskArns": expected_task_arns,
+        "nextToken": next_token,
+    }
     mock_ecs.describe_tasks.return_value = {"tasks": None}
 
     event = {"region": "useast1", "cluster": "cluster"}
     result = lambda_handler(event, {})
 
-    assert result["is_running"] is False
+    assert result["isRunning"] is expected_is_running
+
 
 @patch("boto3.client")
-def test_lambda_handler_others_running(mock_boto_client):
+def test_lambda_handler_when_only_other_groups_running(mock_boto_client):
+    expected_is_running = False
+
     mock_ecs = mock_boto_client.return_value
     mock_ecs.list_tasks.return_value = {"taskArns": ["1", "2"]}
-    mock_ecs.describe_tasks.return_value = {"tasks": [{"group": "other_group"}, {"group": "other_group"}]}
+    mock_ecs.describe_tasks.return_value = {
+        "tasks": [
+            {"taskArn": "1", "group": "other_group"},
+            {"taskArn": "2", "group": "other_group"},
+        ]
+    }
 
     event = {"region": "useast1", "cluster": "cluster", "test_id": "123"}
     result = lambda_handler(event, {})
 
-    assert result["is_running"] is False
+    assert result["isRunning"] is expected_is_running
+
 
 @patch("boto3.client")
-def test_lambda_handler_running(mock_boto_client):
+def test_lambda_handler_when_test_group_running(mock_boto_client):
+    expected_is_running = True
+
     mock_ecs = mock_boto_client.return_value
-    mock_ecs.list_tasks.return_value = {"taskArns": ["1", "2"]}
-    mock_ecs.describe_tasks.return_value = {"tasks": [{"group": "123"}, {"group": "other_group"}]}
+    mock_ecs.list_tasks.return_value = {"taskArns": ["1", "2"], "nextToken": None}
+    mock_ecs.describe_tasks.return_value = {
+        "tasks": [
+            {"taskArn": "1", "group": "123"},
+            {"taskArn": "2", "group": "other_group"},
+        ]
+    }
 
     event = {"region": "useast1", "cluster": "cluster", "test_id": "123"}
     result = lambda_handler(event, {})
 
-    assert result["is_running"] is True
+    assert result["isRunning"] is expected_is_running
+
 
 @patch("boto3.client")
-def test_tasks_are_empty_describe_not_called(mock_boto_client):
+def test_describe_not_calls_with_empty_task_arns(mock_boto_client):
     mock_ecs = mock_boto_client.return_value
-    mock_ecs.list_tasks.return_value = {"taskArns": []}
+    list_tasks_mock: Mock = Mock()
+    mock_ecs.list_tasks = list_tasks_mock
+
+    expected_first_list_task_arns = []
+    expected_first_next_token = "some token"
+
+    expected_second_list_task_arns = []
+    expected_second_next_token = None
+
+    mock_ecs.list_tasks.side_effect = [
+        {
+            "taskArns": expected_first_list_task_arns,
+            "nextToken": expected_first_next_token,
+        },
+        {
+            "taskArns": expected_second_list_task_arns,
+            "nextToken": expected_second_next_token,
+        },
+    ]
 
     event = {"region": "useast1", "cluster": "cluster", "test_id": "123"}
     result = lambda_handler(event, {})
 
-    assert result["is_running"] is False
+    assert result["isRunning"] is False
     mock_ecs.describe_tasks.assert_not_called()
